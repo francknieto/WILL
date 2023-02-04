5ae05edc0b64945c9c42d2c7ecd285399607a1b5
franck <123962728+francknieto@users.noreply.github.com>
2023-02-04 16:25:54 -0500

    Update 03.js

--- 03.js
+++ 03.js
@@ -14,6 +14,7 @@ function buscarAmigo(amigos, nombre) {
   //  buscarAmigo(amigos, 'toni') debe devolver { nombre: 'toni', edad: 33 };
 
   // Tu código aca:
+  console.log(amigos + nombre;) + edad;
   
 }
 
